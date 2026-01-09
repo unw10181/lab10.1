2c76ac07bd727ecb7cddc68eb7e8471d6d9a71d5
unw10181 <uchennanwilliams@gmail.com>
2026-01-08 21:37:49 -0500

    logic for count

--- react-counter/src/components/AdvancedCounter.tsx
+++ react-counter/src/components/AdvancedCounter.tsx
@@ -1,8 +1,10 @@
- const AdvancedCounter = () => {
+import { useEffect, useState } from "react";
+import "../styles/AdvancedCount.css";
 
+const AdvancedCounter = () => {
+  const [count, setCount] = useState<number>(0);
+
+  
 };
 
 export default AdvancedCounter;
-
-
-
