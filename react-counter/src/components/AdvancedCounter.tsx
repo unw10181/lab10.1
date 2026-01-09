f1d0abdd2d59dce62e94656299ad36e72dd87dd0
unw10181 <uchennanwilliams@gmail.com>
2026-01-08 21:38:53 -0500

    more useState hooks

--- react-counter/src/components/AdvancedCounter.tsx
+++ react-counter/src/components/AdvancedCounter.tsx
@@ -3,8 +3,20 @@ import "../styles/AdvancedCount.css";
 
 const AdvancedCounter = () => {
   const [count, setCount] = useState<number>(0);
+  const [history, setHistory] = useState<number[]>([0]);
+  const [step, setStep] = useState<number>(1);
+  const [status, setStatus] = useState<string>("");
 
-  
+  /* -----------------------------
+     Handlers
+  ------------------------------ */
+  const increment = () => {
+    setCount((prev) => prev + step);
+  };
+
+  const decrement = () => {
+    setCount((prev) => prev - step);
+  };
 };
 
 export default AdvancedCounter;
